b8b13f28359777c27efed31165440e9503b2487d
Daniel Hood <danielhood@hotmail.com>
2017-06-06 23:41:24 -0400

    Initial add for basic server access

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "log"
+  "crypto/tls"
+
+  "gopkg.in/resty.v0"
+  "github.com/danielhood/loco.cli/config"
+  "github.com/danielhood/loco.cli/clientApis"
+)
+
+func main() {
+  log.Printf("loco.cli v%v starting", config.Version)
+
+  resty.SetTLSClientConfig(&tls.Config{ InsecureSkipVerify: true })
+
+  log.Print("Getting token from server: ", config.LocoServer)
+
+  token, err := clientApis.GetToken()
+  if (err != nil) {
+    log.Printf("Unable to get token: ", err)
+    return
+  }
+
+  log.Print("Got token: ", token)
+
+  log.Print("Getting object list")
+  objects, err := clientApis.GetObjects(token)
+
+  log.Print("Got Objects: ", objects)
+
+  log.Print("Done")
+}
